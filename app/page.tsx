9c604b121683f350e1526048b21897d9ba6cf7cc
Adair Rohling <rohling@users.noreply.github.com>
2026-05-28 00:06:43 +0000

    feat: dashboard inicial

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <>
+    <h1>Bem Vindo!</h1>
+    <Button variant="secondary" >Salvar</Button>
+    </>
+  );
+}
---
+++ app/student/page.tsx
@@ -0,0 +1,36 @@
+import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, BreadcrumbPage } from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+
+
+export default function ListStudent(){
+    return(
+        <>
+         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-vertical:h-4 data-vertical:self-auto"
+            />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Estudante
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Lista de Estudante</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div>
+            <h1>Lista de Estudantes</h1>
+        </div>
+        </>
+    )
+}
